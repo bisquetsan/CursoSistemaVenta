d1dd3df80c6969f4e2a2398d2ae619fb92b99ef9
Alejandro Bisquet <abisquet@gmail.com>
2025-08-23 19:06:24 -0400

    Categoria en capa de datos

--- CapaEntidad/Categoria.cs
+++ CapaEntidad/Categoria.cs
@@ -9,7 +9,7 @@ namespace CapaEntidad
     public class Categoria
     {
         public int IdCategoria { get; set; }
-        public string Description { get; set; }
+        public string Descripcion { get; set; }
         public bool Estado { get; set; }
         public string FechaRegistro { get; set; }
 
