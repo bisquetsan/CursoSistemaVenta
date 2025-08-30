9702bd961d69115033e3afac540ca05e76940d1d
Alejandro Bisquet <abisquet@gmail.com>
2025-08-30 13:44:28 -0400

    Actualizando modal de proveedor

--- CapaPresentacion/Modales/mdProveedor.Designer.cs
+++ CapaPresentacion/Modales/mdProveedor.Designer.cs
@@ -28,12 +28,198 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "mdProveedor";
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            dgvdata = new DataGridView();
+            Id = new DataGridViewTextBoxColumn();
+            Documento = new DataGridViewTextBoxColumn();
+            RazonSocial = new DataGridViewTextBoxColumn();
+            panel1 = new Panel();
+            btnbusqueda = new FontAwesome.Sharp.IconButton();
+            btnlimpiarbuscador = new FontAwesome.Sharp.IconButton();
+            cbobusqueda = new ComboBox();
+            txtbusqueda = new TextBox();
+            label10 = new Label();
+            label9 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvdata).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dgvdata
+            // 
+            dgvdata.AllowUserToAddRows = false;
+            dgvdata.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dgvdata.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvdata.BackgroundColor = Color.White;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Control;
+            dataGridViewCellStyle1.Font = new Font("Verdana", 10F);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.Padding = new Padding(2);
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dgvdata.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dgvdata.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvdata.Columns.AddRange(new DataGridViewColumn[] { Id, Documento, RazonSocial });
+            dgvdata.Location = new Point(12, 109);
+            dgvdata.Margin = new Padding(100);
+            dgvdata.MultiSelect = false;
+            dgvdata.Name = "dgvdata";
+            dgvdata.ReadOnly = true;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle2.SelectionBackColor = Color.RoyalBlue;
+            dataGridViewCellStyle2.SelectionForeColor = Color.White;
+            dgvdata.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            dgvdata.RowTemplate.Height = 28;
+            dgvdata.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvdata.Size = new Size(527, 236);
+            dgvdata.TabIndex = 35;
+            dgvdata.CellDoubleClick += dgvdata_CellDoubleClick;
+            // 
+            // Id
+            // 
+            Id.HeaderText = "Id";
+            Id.Name = "Id";
+            Id.ReadOnly = true;
+            Id.Visible = false;
+            // 
+            // Documento
+            // 
+            Documento.FillWeight = 120F;
+            Documento.HeaderText = "Nro Documento";
+            Documento.Name = "Documento";
+            Documento.ReadOnly = true;
+            // 
+            // RazonSocial
+            // 
+            RazonSocial.FillWeight = 120F;
+            RazonSocial.HeaderText = "Razón Social";
+            RazonSocial.Name = "RazonSocial";
+            RazonSocial.ReadOnly = true;
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(btnbusqueda);
+            panel1.Controls.Add(btnlimpiarbuscador);
+            panel1.Controls.Add(cbobusqueda);
+            panel1.Controls.Add(txtbusqueda);
+            panel1.Controls.Add(label10);
+            panel1.Controls.Add(label9);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(527, 87);
+            panel1.TabIndex = 36;
+            // 
+            // btnbusqueda
+            // 
+            btnbusqueda.BackColor = Color.ForestGreen;
+            btnbusqueda.Cursor = Cursors.Hand;
+            btnbusqueda.FlatAppearance.BorderColor = Color.Black;
+            btnbusqueda.FlatStyle = FlatStyle.Flat;
+            btnbusqueda.ForeColor = Color.White;
+            btnbusqueda.IconChar = FontAwesome.Sharp.IconChar.Search;
+            btnbusqueda.IconColor = Color.White;
+            btnbusqueda.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnbusqueda.IconSize = 20;
+            btnbusqueda.Location = new Point(400, 50);
+            btnbusqueda.Name = "btnbusqueda";
+            btnbusqueda.Size = new Size(49, 25);
+            btnbusqueda.TabIndex = 25;
+            btnbusqueda.TextAlign = ContentAlignment.MiddleRight;
+            btnbusqueda.UseVisualStyleBackColor = false;
+            btnbusqueda.Click += btnbusqueda_Click;
+            // 
+            // btnlimpiarbuscador
+            // 
+            btnlimpiarbuscador.BackColor = Color.RoyalBlue;
+            btnlimpiarbuscador.Cursor = Cursors.Hand;
+            btnlimpiarbuscador.FlatAppearance.BorderColor = Color.Black;
+            btnlimpiarbuscador.FlatStyle = FlatStyle.Flat;
+            btnlimpiarbuscador.ForeColor = Color.White;
+            btnlimpiarbuscador.IconChar = FontAwesome.Sharp.IconChar.Broom;
+            btnlimpiarbuscador.IconColor = Color.White;
+            btnlimpiarbuscador.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnlimpiarbuscador.IconSize = 20;
+            btnlimpiarbuscador.Location = new Point(455, 50);
+            btnlimpiarbuscador.Name = "btnlimpiarbuscador";
+            btnlimpiarbuscador.Size = new Size(49, 25);
+            btnlimpiarbuscador.TabIndex = 26;
+            btnlimpiarbuscador.TextAlign = ContentAlignment.MiddleRight;
+            btnlimpiarbuscador.UseVisualStyleBackColor = false;
+            btnlimpiarbuscador.Click += btnlimpiarbuscador_Click;
+            // 
+            // cbobusqueda
+            // 
+            cbobusqueda.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbobusqueda.Font = new Font("Segoe UI", 10F);
+            cbobusqueda.FormattingEnabled = true;
+            cbobusqueda.Location = new Point(119, 50);
+            cbobusqueda.Name = "cbobusqueda";
+            cbobusqueda.Size = new Size(133, 25);
+            cbobusqueda.TabIndex = 22;
+            // 
+            // txtbusqueda
+            // 
+            txtbusqueda.Font = new Font("Segoe UI", 10F);
+            txtbusqueda.Location = new Point(258, 50);
+            txtbusqueda.Name = "txtbusqueda";
+            txtbusqueda.Size = new Size(136, 25);
+            txtbusqueda.TabIndex = 24;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.White;
+            label10.Font = new Font("Segoe UI", 10F);
+            label10.Location = new Point(39, 54);
+            label10.Name = "label10";
+            label10.Size = new Size(77, 19);
+            label10.TabIndex = 23;
+            label10.Text = "Buscar por:";
+            // 
+            // label9
+            // 
+            label9.BackColor = Color.White;
+            label9.Font = new Font("Segoe UI", 17F);
+            label9.Location = new Point(0, 0);
+            label9.Name = "label9";
+            label9.Size = new Size(244, 36);
+            label9.TabIndex = 20;
+            label9.Text = "Lista de Proveedores";
+            label9.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // mdProveedor
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(551, 356);
+            Controls.Add(dgvdata);
+            Controls.Add(panel1);
+            Name = "mdProveedor";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "mdProveedor";
+            Load += mdProveedor_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvdata).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dgvdata;
+        private Panel panel1;
+        private FontAwesome.Sharp.IconButton btnbusqueda;
+        private FontAwesome.Sharp.IconButton btnlimpiarbuscador;
+        private ComboBox cbobusqueda;
+        private TextBox txtbusqueda;
+        private Label label10;
+        private Label label9;
+        private DataGridViewTextBoxColumn Id;
+        private DataGridViewTextBoxColumn Documento;
+        private DataGridViewTextBoxColumn RazonSocial;
     }
 }
--- CapaPresentacion/Modales/mdProveedor.cs
+++ CapaPresentacion/Modales/mdProveedor.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CapaEntidad;
+using CapaNegocio;
+using CapaPresentacion.Utilidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,80 @@ namespace CapaPresentacion.Modales
 {
     public partial class mdProveedor : Form
     {
+        public Proveedor _Proveedor { get; set; }
         public mdProveedor()
         {
             InitializeComponent();
+        }
+
+        private void mdProveedor_Load(object sender, EventArgs e)
+        {
+            foreach (DataGridViewColumn columna in dgvdata.Columns)
+            {
+                if (columna.Visible == true && columna.Name != "btnseleccionar")
+                {
+                    cbobusqueda.Items.Add(new OpcionCombo() { Valor = columna.Name, Texto = columna.HeaderText });
+                }
+            }
+            cbobusqueda.DisplayMember = "Texto";
+            cbobusqueda.ValueMember = "Valor";
+            cbobusqueda.SelectedIndex = 0;
+
+            List<Proveedor> lista = new CN_Proveedor().Listar();
+            foreach (Proveedor item in lista)
+            {
+                dgvdata.Rows.Add(new object[] {
+                    item.IdProveedor,
+                    item.Documento,
+                    item.RazonSocial,
+                });
+            }
+        }
+
+        private void btnbusqueda_Click(object sender, EventArgs e)
+        {
+            string columnafiltro = ((OpcionCombo)cbobusqueda.SelectedItem).Valor.ToString();
+            if (dgvdata.Rows.Count > 0)
+            {
+                foreach (DataGridViewRow row in dgvdata.Rows)
+                {
+                    if (row.Cells[columnafiltro].Value.ToString().Trim().ToUpper().Contains(txtbusqueda.Text.Trim().ToUpper()))
+                    {
+                        row.Visible = true;
+                    }
+                    else
+                        row.Visible = false;
+                }
+            }
+
+        }
+
+        private void btnlimpiarbuscador_Click(object sender, EventArgs e)
+        {
+            txtbusqueda.Text = "";
+            foreach (DataGridViewRow row in dgvdata.Rows)
+            {
+                row.Visible = true;
+            }
+        }
+
+        private void dgvdata_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int iRow = e.RowIndex;
+            int iCol = e.ColumnIndex;
+
+            if (iRow >= 0 && iCol > 0)
+            {
+                _Proveedor = new Proveedor() {
+                    IdProveedor = Convert.ToInt32(dgvdata.Rows[iRow].Cells["id"].Value.ToString()),
+                    Documento = dgvdata.Rows[iRow].Cells["Documento"].Value.ToString(),
+                    RazonSocial = dgvdata.Rows[iRow].Cells["RazonSocial"].Value.ToString(),
+                };
+
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+
+            }
         }
     }
 }
